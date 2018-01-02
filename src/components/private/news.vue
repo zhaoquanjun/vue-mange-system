85f9731e78c8041e8238c6f13f3196def0860f8e
zhaoquanjun <zhaoquanjun0@qq.com>
2018-01-02 18:02:32 +0800

    add card

--- src/components/private/news.vue
+++ src/components/private/news.vue
@@ -3,12 +3,23 @@
         <h2 class="title">近期新事件</h2>
         <el-card class="box-card">
             <div slot="header">
-                <span>追剧小美好，就是这么爱你</span>
+                <span class="cardTitle">追剧小美好，就是这么爱你</span>
                 <el-button style="float:right;padding:3px" type="text" @click="submit">操作按钮</el-button>
-                <i class="el-icon-star-off" v-if="startShow" @click="star" style="display:block;float:right;padding-right:20px;padding-top:2px"></i>
-                <i class="el-icon-star-on" v-else  @click="star" style="display:block;float:right;padding-right:20px;padding-top:2px"></i>
+                <i class="el-icon-star-off" v-if="star1Show" @click="news1Star" style="display:block;float:right;padding-right:20px;padding-top:2px"></i>
+                <i class="el-icon-star-on" v-else  @click="news1Star" style="display:block;float:right;padding-right:20px;padding-top:2px"></i>
                 </div>
             <div class="text item" v-for="item in news1" :key="item.desc">
+                <router-link :to="'/newsDetail' + item.path">{{item.title}}</router-link>
+            </div>
+        </el-card>
+        <el-card class="box-card">
+            <div slot="header">
+                <span class="cardTitle">告别2017，面对2018，你有什么想法~</span>
+                <el-button style="float:right;padding:3px" type="text" @click="submit">操作按钮</el-button>
+                <i class="el-icon-star-off" v-if="star2Show" @click="news2Star" style="display:block;float:right;padding-right:20px;padding-top:2px"></i>
+                <i class="el-icon-star-on" v-else  @click="news2Star" style="display:block;float:right;padding-right:20px;padding-top:2px"></i>
+                </div>
+            <div class="text item" v-for="item in news2" :key="item.desc">
                 <router-link :to="'/newsDetail' + item.path">{{item.title}}</router-link>
             </div>
         </el-card>
@@ -51,12 +62,48 @@ export default {
           path: "/shenyueyue_fanwai?news1_01"
         }
       ],
-      startShow: true
+      news2: [
+        {
+          title: "踩着2017的尾巴来到了2018，对于新的一年，你有什么想法",
+          desc: "new2018",
+          path: "/new2018?news2_01"
+        },
+        {
+          title: "对于过去的2017，有几多感悟",
+          desc: "past2017",
+          path: "/past2017?news2_02"
+        },
+        {
+          title: "对于这个新来的2018，你对自己的职业生涯是不是有了更深的认识呢？",
+          desc: "2018",
+          path: "/idea_of_carceer?news2_03"
+        },
+        {
+          title: "经历了这么许久的一个人的生活，是否应该彻底脱离曾经的阴影，寻求一个新的生活了呢？",
+          desc: "outSignal",
+          path: "/outSignal?news2_04"
+        },
+        {
+          title: "对于曾经的美好，适时的画上一个完整的句号，是对于以后生活的一种负责的态度",
+          desc: "outOfPast",
+          path: "/outOfPast?news2_05"
+        },
+        {
+          title: "几年折腾，稚气、冲动都已经不再适合我们了~",
+          desc: "adultness",
+          path: "/adultness?news2_06"
+        }
+      ],
+      star1Show: true,
+      star2Show: true
     };
   },
   methods: {
-    star() {
-      this.startShow = !this.startShow;
+    news1Star() {
+      this.star1Show = !this.star1Show;
+    },
+    news2Star() {
+      this.star2Show = !this.star2Show;
     },
     submit() {
       this.$message("目前没事干");
@@ -89,6 +136,11 @@ export default {
   clear: both;
 }
 .box-card {
+  float: left;
   width: 45%;
+  margin-left: 4%;
+}
+.cardTitle {
+  color: deepskyblue;
 }
 </style>
